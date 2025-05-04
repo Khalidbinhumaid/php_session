8896586aa563827aea6cac39d09668624bc535ec
khalidbinhumaid <k.binhumaid@bimventures.com>
2025-05-04 11:42:49 +0300

    pushing changes

--- all_in_one.php
+++ all_in_one.php
@@ -4,10 +4,22 @@
 
 <head></head>
 <body>
-<h1>This is a string, variable, integer, and float </h1>
+<h1>This is a variable , string,  integer, and float </h1>
+<?php
+$name= "khalid";      // string variable
+$age = 25;        // integer variable
+$height = 5.9;       // float variable
+
+
+echo "String: $name <br>";
+echo "integer: $age <br>";
+echo "float: $height <br>";
 
 
 
+
+
+?>
 
 
 
