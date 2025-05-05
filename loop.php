5c06e6637c74989412949422ded5dbfeef914a80
khalidbinhumaid <k.binhumaid@bimventures.com>
2025-05-05 12:14:38 +0300

    loop

---
+++ loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html> 
+<head></head>
+<body>
+
+
+<h1>for loop </h1>
+<?php 
+for ($i=0; $i <100;$i++){
+    if ($i == 23){
+        echo "Count : {$i}";
+        break;
+    }
+}
+
+
+
+?>
+
+</body>
+
+
+</html>
