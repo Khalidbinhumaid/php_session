874a58a23442816d37f841db82c98732311f0c5e
khalidbinhumaid <k.binhumaid@bimventures.com>
2025-05-06 13:04:53 +0300

    contactus

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact </title>
+
+    <h1>you can contact us </h1>
+</head>
+<body>
+    <?php
+
+    //echo print_r($_GET);
+?>
+</br>
+ID:<?php echo $_GET['id']; ?></br>
+name:<?php echo $_GET['name']; ?>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main page</title>
+    <h1>Welcome to main page</h1>
+</head>
+<body>
+    <?php 
+    
+    $urltext="contact us"
+    
+    
+    
+    
+    
+    
+    
+    
+    ?>
+
+
+<a href='contact.php?id=100&name=khalid'>Contact us</a>
+</body>
+</html>
